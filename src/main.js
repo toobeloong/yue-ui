a33d115fa473b16136c0548bd190e31551995ab2
Harry Rong <harrykobe@gmail.com>
2016-06-11 12:53:59 +0800

    此提交开始默认该仓库使用中文，本次提交更新一个可用版本

--- src/main.js
+++ src/main.js
@@ -1,11 +1,11 @@
-import yButton from './button/button.vue'
+import yueButton from './button/button.vue'
 
 let components = {
-	yButton: yButton
+	yueButton: yueButton
 };
 
 export function install(vue){
 	for (let name in components) {
-		Vue.component(name, components[name]);
+		vue.component(name, components[name]);
 	}
 };
